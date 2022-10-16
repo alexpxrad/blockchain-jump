525a18eb63d28547fefb099f9f01680b67aa4ffd
alexpxrad <alexpxrad@gmail.com>
2022-10-16 17:00:58 -0400

    game elements being set up

---
+++ nft.js
@@ -0,0 +1,52 @@
+import {
+    setCustomProperty,
+    incrementCustomProperty,
+    getCustomProperty
+} from "./updateCustomProperty.js"
+
+const SPEED = 0.05
+const OBSTACLE_INTERVAL_MIN = 500
+const OBSTACLE_INTERVAL_MAX = 2000
+const gameElem = document. querySelector("[data-game]")
+
+let nextNFTTime 
+export function setupNFT() {
+
+    nextNFTTime = OBSTACLE_INTERVAL_MIN
+
+    document.querySelectorAll('[data-nft]').forEach(nft => {
+        nft.remove
+    })
+}
+
+export function updateNft(delta, speedScale) {
+    document.querySelectorAll("[data-nft]").forEach(nft => {
+        incrementCustomProperty(nft, "--left", delta * speedScale * SPEED * -1)
+        if (getCustomProperty(nft, "--left") <= -100) {
+            nft.remove()
+        }
+    })
+
+    if (nextNFTTime <= 0) {
+        createNFT()
+        nextNFTTime =
+        randomNumberBetween(OBSTACLE_INTERVAL_MIN, OBSTACLE_INTERVAL_MAX) / speedScale
+    }
+
+    nextNFTTime -= delta
+}
+
+function createNFT() {
+    const nft = document.createElement('img')
+    nft.dataset.nft = true
+
+    nft.src = "imgs/nft.png"
+
+    nft.classList.add('nft')
+    setCustomProperty(nft, "--left", 100)
+    gameElem.append(nft)
+}
+
+function randomNumberBetween(min, max) {
+    return Math.floor(Math.random() * (max - min + 1 ) + min)
+}
