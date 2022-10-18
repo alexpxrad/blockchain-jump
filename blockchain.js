3e5ac9021ffb105c4530a759650eabf55903e90e
alexpxrad <alexpxrad@gmail.com>
2022-10-18 01:44:18 -0400

    fixed issue in blockchain.js

--- blockchain.js
+++ blockchain.js
@@ -1,5 +1,4 @@
 let provider = new ethers.providers.Web3Provider(window.ethereum)
-
 let signer
 
 async function connectMetamask() {
@@ -9,28 +8,25 @@ async function connectMetamask() {
 
     const balance = await signer.getBalance()
     const convertToEth = 1e18;
-    console.log("account's balance in ether:", balance.toString() / convertToEth)
-
+    console.log("account's balance in ether:", balance.toString() / convertToEth);
 }
-
 
 async function claimTokens() {
     const runTokenContractAddress = "0x0950a2e33520D15b02A7d666fA017D98947aAEc2";
     const runTokenContractAbi = [
-            "function mintTokens(address account, uint256, amount) public"
+        "function mintTokens(address account, uint256 amount) public",
     ];
-
     const runTokenContract = new ethers.Contract(runTokenContractAddress, runTokenContractAbi, provider);
     let convertToWei = 1000000000
     let amountToClaim = window.totalGweiScore * convertToWei
     await runTokenContract.connect(signer).mintTokens(signer.getAddress(), amountToClaim.toString())
 }
 
-async function claimNFT() {
+async function claimNft() {
     const nftContractAddress = "0xD9B50248fdcFd297770e89cf318D71E2B63d1c01";
     const mintContractAbi = [
-        "function mint(uint256 amount) public"
+        "function mint(uint256 amount) public",
     ];
-        const nftContract = new ethers.Contract(nftContractAddress, mintContractAbi, provider)
-        await nftContract.connect(signer).mint(window.totalNFTScore.toString())
+    const nftContract = new ethers.Contract(nftContractAddress, mintContractAbi, provider);
+    await nftContract.connect(signer).mint(window.totalNFTScore.toString())
 }
